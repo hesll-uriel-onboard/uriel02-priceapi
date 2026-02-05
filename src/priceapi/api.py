dd2c786da0c3ca36155cb309eb033c07655a5d8a
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2026-02-05 16:25:23 +0700

    add end_time

--- src/priceapi/api.py
+++ src/priceapi/api.py
@@ -2,14 +2,15 @@ from datetime import datetime
 import requests
 from .models.price import Price
 
-def get_price(coin: str, interval: str, limit: int = 2):
-	URL = f"https://api.binance.com/api/v3/klines?symbol={coin}USDT&interval={interval}&limit={limit}"
-	req = requests.get(url=URL)
+def get_price(coin: str, interval: str, end_time: datetime, limit: int = 2):
+	timestamp = int(end_time.timestamp() * 1000) # milli
+	URL = f"https://api.binance.com/api/v3/klines?symbol={coin}USDT&interval={interval}&limit={limit}&endTime={timestamp}"
+	req = requests.get(url=URL, timeout=5)
 	return [Price(coin, point) for point in req.json()]
 
-def duys_strategy():
+def duys_strategy(end_time: datetime):
 	FORMAT = "%H:%M:%S"
-	p1, p2 = get_price("BTC", "1m")
+	p1, p2 = get_price("BTC", "1m", end_time)
 	return {
 		"time": f"{datetime.fromtimestamp(p1.time_opened).strftime(FORMAT)} -> {datetime.fromtimestamp(p2.time_closed).strftime(FORMAT)}",
 		"price": p2.price_closed,
@@ -17,5 +18,5 @@ def duys_strategy():
 	}
 
 if __name__ == "__main__":
-	res = get_price("BTC", "1m")
+	res = get_price("BTC", "1m", datetime.now())
 	print(*res)
