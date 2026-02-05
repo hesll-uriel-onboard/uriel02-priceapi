d06f6db20f30fc91c4b776fdd965c02df21fc15d
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2026-02-05 23:37:26 +0700

    add return type

--- src/priceapi/api.py
+++ src/priceapi/api.py
@@ -2,19 +2,19 @@ from datetime import datetime
 import requests
 from .models.price import Price
 
-def get_price(coin: str, interval: str, end_time: datetime, limit: int = 2):
+def get_price(coin: str, interval: str, end_time: datetime, limit: int = 2) -> list[Price]:
 	timestamp = int(end_time.timestamp() * 1000) # milli
 	URL = f"https://api.binance.com/api/v3/klines?symbol={coin}USDT&interval={interval}&limit={limit}&endTime={timestamp}"
 	req = requests.get(url=URL, timeout=5)
 	return [Price(coin, point) for point in req.json()]
 
-def duys_strategy(end_time: datetime):
+def duys_strategy(end_time: datetime) -> dict[str, str]:
 	FORMAT = "%H:%M:%S"
 	p1, p2 = get_price("BTC", "1m", end_time)
 	return {
 		"time": f"{datetime.fromtimestamp(p1.time_opened).strftime(FORMAT)} -> {datetime.fromtimestamp(p2.time_closed).strftime(FORMAT)}",
-		"price": p2.price_closed,
-		"coef": p2.price_closed / p1.price_opened
+		"price": str(p2.price_closed),
+		"coef": str(p2.price_closed / p1.price_opened)
 	}
 
 if __name__ == "__main__":
