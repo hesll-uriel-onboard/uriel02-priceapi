0c114452f7dbdeaeebdebe333345e0a9bcb295ca
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2026-02-11 17:55:07 +0700

    update test implementation

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from priceapi.client import Client, ClientProvider
 from priceapi.service import ServiceInterface, milli_timestamp
 
-
+VALID_SYMBOLS = "BTC_USDT"
+VALID_INTERVAL = "15m"
 def test_OK():
 	end_time = datetime.now()
 	for provider in ClientProvider:
 		client = Client()
-		result = client.get_finished_candles("BTC-USDT", "1m", end_time, provider = provider)
+		result = client.get_finished_candles(VALID_SYMBOLS, VALID_INTERVAL, end_time, provider = provider)
 		for candle in result:
 			assert candle.time_closed <= milli_timestamp(end_time)
 
@@ -15,7 +16,7 @@ def test_wrong_symbols():
 	for provider in ClientProvider:
 		client = Client()
 		try:
-			client.get_finished_candles("ABD-EGH", "1m", datetime.now(), provider = provider)
+			client.get_finished_candles("ABD-E?GH", VALID_INTERVAL, datetime.now(), provider = provider)
 		except ServiceInterface.InvalidParameterError:
 			pass
 		except Exception:
@@ -25,7 +26,7 @@ def test_wrong_interval():
 	for provider in ClientProvider:
 		client = Client()
 		try:
-			client.get_finished_candles("ABD-EGH", "7m", datetime.now(), provider = provider)
+			client.get_finished_candles(VALID_SYMBOLS, "7m", datetime.now(), provider = provider)
 		except ServiceInterface.InvalidParameterError:
 			pass
 		except Exception:
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,18 +1,20 @@
 from datetime import datetime
-from time import sleep
 from priceapi.models.interval import Interval
-from priceapi.service import BinanceService, ServiceInterface, milli_timestamp
+from priceapi.service import BinanceService, ServiceInterface
 
+VALID_BASE = "BTC"
+VALID_QUOTE = "USDT"
+VALID_INTERVAL = Interval("15m")
 def test_binance_service_OK():
 	service = BinanceService()
 	end_time = datetime.now()
-	result = service.get_finished_candles("BTC", "USDT", Interval("15m"), end_time)
+	result = service.get_finished_candles(VALID_BASE, VALID_QUOTE, VALID_INTERVAL, end_time)
 	assert len(result) <= 2
 
 def test_binance_invalid_symbols():
 	service = BinanceService()
 	try:
-		service.get_finished_candles("ABC", "DEF", Interval("15m"), datetime.now())
+		service.get_finished_candles("ABC", "DEF", VALID_INTERVAL, datetime.now())
 	except ServiceInterface.InvalidParameterError:
 		assert True
 	except Exception as e:
@@ -22,7 +24,7 @@ def test_binance_invalid_symbols():
 def test_binance_invalid_interval():
 	service = BinanceService()
 	try:
-		service.get_finished_candles("BTC", "USDT", Interval("7m"), datetime.now())
+		service.get_finished_candles(VALID_BASE, VALID_QUOTE, Interval("7m"), datetime.now())
 	except ServiceInterface.InvalidParameterError:
 		assert True
 	except Exception as e:
